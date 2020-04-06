e4de3b001bdd9094af1b7167fdade87cc388ee0b
Hans Andreas <hans.matthias.a@gmail.com>
2020-04-06 14:00:56 +0300

    Added buyAll() + cart system

--- TicketProject/src/main/java/Client.java
+++ TicketProject/src/main/java/Client.java
@@ -5,6 +5,7 @@ public class Client extends Person {
     boolean yes_mail; //Kas soovib emailiga piletit?
     private double accountBalance; //Konto
     private List<Ticket> cart; //Ostukorv
+    private double cartPrice;
     private List<Ticket>purchased; //FIXME: Ei tea veel kas läheb tarvis.
 
     public Client(String eesnimi, String perenimi, String sugu, int vanus, String email, long telefon, String aadress, String maakond, long indeks, boolean yes_mail,double accountBalance) {
@@ -14,7 +15,14 @@ public class Client extends Person {
         cart=new ArrayList<Ticket>();
         purchased=new ArrayList<Ticket>();
     }
-
+    public void addToList(Ticket ticket) {
+        cartPrice += ticket.getPrice();
+        cart.add(ticket);
+    }
+    public void removeFromList(Ticket ticket){
+        cartPrice-= ticket.getPrice();
+        cart.remove(ticket);
+    }
     //Maksmise meetod.
     public boolean buy(Ticket ticket){
 
@@ -39,6 +47,23 @@ public class Client extends Person {
     }
 
     public boolean buyAll(){   //TODO!: Cartist ostmine, carti täishinna arvutamine.
-        return false;
+        if (accountBalance >= cartPrice && !cart.isEmpty()) {
+            Owner thisOwner = cart.get(0).getOwner();
+            accountBalance -= cartPrice;
+            thisOwner.addToAccount(cartPrice);
+            StringBuilder cartSum = new StringBuilder();
+            for (int i = 0; i < cart.size(); i++) {
+                cartSum.append(cart.get(i).getName()).append("\t");
+                purchased.add(cart.get(i)); //FIXME: Ei tea veel kas läheb tarvis.
+            }
+            thisOwner.addLog(getName() + " " + getFamilyName() + " ostis: " + cartSum + " hinna eest: " + cartPrice);
+            addLog("Ostmine õnnestus: " + cartSum);
+            return true;
+        } else {
+            // Ei tohiks saada buyAll() peale üldse vajutada kui pole midagi cartis ma arvan
+            // Võin siia midagi lisada kui vaja.
+            addLog("Ostmine ebaõnnestus: Kontol pole piisavalt raha!");
+            return false;
+        }
     }
 }
