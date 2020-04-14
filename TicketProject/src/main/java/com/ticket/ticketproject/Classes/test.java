9ca122e2240e002fec91d8fa6953a02323ab257b
Hans Andreas <hans.matthias.a@gmail.com>
2020-04-14 18:08:30 +0300

    Files organized + TicketController

--- TicketProject/src/main/java/com/ticket/ticketproject/Classes/test.java
+++
@@ -1,7 +0,0 @@
-package com.ticket.ticketproject.Classes;
-
-public class test {
-    public static void main(String[] args) {
-        Email.email();
-    }
-}
--- TicketProject/src/test/java/com/ticket/ticketproject/TicketprojectApplicationTests.java
+++ TicketProject/src/test/java/com/ticket/ticketproject/TicketprojectApplicationTests.java
@@ -2,8 +2,13 @@ package com.ticket.ticketproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootTest
+@EnableJpaRepositories(basePackages = {"com.ticket.controllers", "com.ticket.dataStorage","com.ticket.functionalities"})
+@ComponentScan(basePackages = {"com.ticket.controllers", "com.ticket.dataStorage","com.ticket.functionalities"})
 class TicketprojectApplicationTests {
 
 	@Test
